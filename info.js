5014965eca7a360345623fe41329b42c322b1691
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-02-12 09:44:59 +0100

    Nettoyage du code

--- info.js
+++ info.js
@@ -1,7 +1,3 @@
-// const test = document.querySelector('#croute-continentale')
-// test.addEventListener("mouseover", () => {
-//     console.log("test")
-// })
 const css = document.documentElement.style
 
 // Chargement des données
@@ -10,7 +6,6 @@ fetch("data.json")
     .then(response => response.json())
     .then(function(response) {
         data = response
-        console.log(data)
     })
     .catch(error => console.log("Erreur: " + error))
 
@@ -22,35 +17,10 @@ function infoBlocAnim() {
     }, 1010);
 }
 
-const outputDiv = document.getElementById('infos');
-// let questionNbr = 0;
-// let divQuizz = document.getElementById('quizz');
-// let inputQuizz = document.querySelectorAll('#quizz input');
-
-// function startQuizz() {
-//     infoBlocAnim()
-
-//     document.getElementById('infos-discontinuite').hidden = true;
-//     document.getElementById('infos-couche').hidden = true;
-//     divQuizz.hidden = false;
-//     questionNbr = 0;
-
-// }
-
-// function checkResponse() {
-//     for (i = 0; i < inputQuizz.length; i++) {
-//         if (inputQuizz[i].value === data[0]) {
-//             console.log("OK", data[1])
-//         } else {
-//             console.log("ERREUR", data[0])
-//         }
-//     }
-// }
-
 let quizzMode = false;
 
 function switchMode() {
-    clearQuizz(oldElement, oldElementFill)
+    clearQuizz(oldElement)
     quizzMode = !quizzMode;
     if (quizzMode) {
         css.setProperty('--switch-value', '30px');
@@ -61,30 +31,35 @@ function switchMode() {
     }
 }
 
+const listOfDiv = [
+    document.getElementById('infos-couche'),
+    document.getElementById('infos-discontinuite'),
+    document.getElementById('quizz-couche'),
+    document.getElementById('quizz-discontinuite')
+    ];
+function affichage(outputID) {
+    for (let i in listOfDiv) {
+        listOfDiv[i].hidden = true;
+    };
+    document.getElementById(outputID).hidden = false;
+}
 
 // Affichage des données spécifiques sur la page
 function loadInformations(elementID) {
 
     // Si le mode quizz est activé, on termine l'execution de la fonction avant l'affichage
     if (quizzMode) {
-        startQuizz(elementID)
+        startQuizz(elementID);
         return
-    }
+    };
 
 // Animations
-    infoBlocAnim()
+    infoBlocAnim();
     
-    console.log(elementID);
-
 // Affichage des infos
 
-    if  (data[elementID].couche) { // vérifie qu'il ne s'agit pas d'une discontinuitée
-        console.log(data[elementID].densite, ",", data[elementID].roches, ",", data[elementID].etat);
-
-        document.getElementById('infos-discontinuite').hidden = true;
-        document.getElementById('quizz').hidden = true;
-        let output = document.getElementById('infos-couche');
-        output.hidden = false;
+    if  (data[elementID].couche) { // vérifie qu'il ne s'agit pas d'une discontinuité
+        affichage('infos-couche');
 
         document.querySelector("#infos-couche #nom").innerHTML = data[elementID].nom;
         document.querySelector("#infos-couche #densite").innerHTML = data[elementID].densite;
@@ -92,14 +67,10 @@ function loadInformations(elementID) {
         document.querySelector("#infos-couche #etat").innerHTML = data[elementID].etat;
 
     } else { // sinon c'est une discontinuité
-        console.log(data[elementID].profondeur);
-        document.getElementById('infos-couche').hidden = true;
-        document.getElementById('quizz').hidden = true;
-        let output = document.getElementById('infos-discontinuite');
-        output.hidden = false;
+        affichage('infos-discontinuite');
 
         document.querySelector("#infos-discontinuite #nom").innerHTML = data[elementID].nom;
         document.querySelector("#infos-discontinuite #profondeur").innerHTML = data[elementID].profondeur;
-    }
+    };
     
 }
