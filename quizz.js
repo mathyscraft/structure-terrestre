6e5aae0e815bb888d12a34ba39876834f12a9954
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-02-12 09:45:38 +0100

    nottoyage du code

--- quizz.js
+++ quizz.js
@@ -2,16 +2,19 @@ function infoBlocAnim() {
     document.getElementById("infoBloc").style.opacity = "1";
     document.querySelector("#underline").style.animation = "apparition-barre 1s";
     setTimeout(() => {
-        document.querySelector("#underline").style.animation = ""
+        document.querySelector("#underline").style.animation = "";
     }, 1010);
 }
 
-function clearQuizz(oldElement, oldfill) {
+let inputQuizz = document.querySelectorAll('#quizz input');
+
+let oldElement= undefined; // Correspond à l'ancienne couche qui a été sélectionnée
+function clearQuizz(oldElement) {
     if(oldElement !== undefined) {
-        if (oldfill){
-        oldElement.style.fill = "";
+        if (oldElement.style.fill === '#f1f1f1' || oldElement.style.fill === 'rgb(241, 241, 241)')  {
+            oldElement.style.fill = "";
         }else{
-        oldElement.style.stroke = "";
+            oldElement.style.stroke = "";
         }
     }
     let oldAnswers = document.querySelectorAll('.correct-answer')
@@ -19,45 +22,29 @@ function clearQuizz(oldElement, oldfill) {
         oldAnswers[i].innerHTML = "";
     }
     for (i=0; i<inputQuizz.length;i++) {
-        inputQuizz[i].style.background = "#f1f1f1"
+        inputQuizz[i].style.background = "#f1f1f1";
     }
 }
 
-let questionNbr = 0;
 let answers = [];
-let divQuizz = document.getElementById('quizz');
-let inputQuizz = document.querySelectorAll('#quizz input');
+function startQuizz(element) { // element correspond à l'ID de la couche
 
-let oldElement= undefined;
-let oldElementFill;
-function startQuizz(element) {
-
-    clearQuizz(oldElement, oldElementFill)
-    oldElement = document.getElementById(element);
-    infoBlocAnim()
-    console.log(element)
-    answers = []
+    clearQuizz(oldElement);
+    oldElement = document.getElementById(element); // Redéfinie l'ancien élément avec l'actuel
+    infoBlocAnim();
+    answers = [];
     
-    document.getElementById('infos-discontinuite').hidden = true;
-    document.getElementById('infos-couche').hidden = true;
-    divQuizz.hidden = false;
-    questionNbr = 0;
-
     if (data[element].couche) {
-        document.getElementById(element).style.fill = '#f1f1f1';
-        oldElementFill=true
-        document.getElementById('quizz-couche').hidden = false;
-        document.getElementById('quizz-discontinuite').hidden = true;
+        document.getElementById(element).style.fill = '#f1f1f1'; 
+        affichage('quizz-couche');
     } else {
         document.getElementById(element).style.stroke = '#f1f1f1';
-        oldElementFill=false
-        document.getElementById('quizz-couche').hidden = true;
-        document.getElementById('quizz-discontinuite').hidden = false;
+        affichage('quizz-discontinuite');
     }
 
     for (i in data[element]) {
         if (i !== "densite" && i !==  "couche") {
-        answers.push(data[element][i])
+        answers.push(data[element][i]);
         }
     }
 
@@ -67,10 +54,8 @@ function checkResponse() {
     for (i = 0; i < answers.length; i++) {
         if (inputQuizz[i].value === answers[i]) {
             inputQuizz[i].style.background ="#64e964";
-            console.log("OK")
         } else {
             inputQuizz[i].style.background ="#d34b4b";
-            console.log("ERREUR", answers[i])
             let correctAnswer = inputQuizz[i].nextElementSibling;
             correctAnswer.innerHTML = answers[i]
         }
